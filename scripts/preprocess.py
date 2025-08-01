c9048169cd3d36ab471503d1716eeb4107596f5d
Kawu <138832632+jharodiv@users.noreply.github.com>
2025-08-01 22:53:49 +0800

    - Loaded the .wav files from wake/, not_wake/, and noise/
    - Computed spectogram features
    
    - Saved them to .npy files for model training

---
+++ scripts/preprocess.py
@@ -0,0 +1,44 @@
+import os 
+import librosa
+import numpy as np
+import soundfile as sf
+from tqdm import tqdm
+
+SAMPLERATE = 16000
+DURATION = 2
+N_MELS = 40
+DATADIR ='../data'
+OUTDIR = '../features'
+
+
+classes = ['wake','not_wake','noise']
+
+os.makedirs(OUTDIR, exist_ok=True)
+
+def extractFeatures(file_path):
+    y, sr = librosa.load(file_path, sr = SAMPLERATE)
+    if len(y) < SAMPLERATE * DURATION:
+        pad = SAMPLERATE * DURATION - len(y)
+        y = np.pad(y, (0, pad))
+    else:
+        y = y[:SAMPLERATE * DURATION]
+    mel = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=N_MELS)
+    meldb = librosa.power_to_db(mel, ref=np.max)
+    return meldb
+
+data = []
+labels = []
+
+
+for label in classes:
+    classdir = os.path.join(DATADIR, label)
+    for file in tqdm(os.listdir(classdir), desc=label):
+        if file.endswith('.was'):
+            path = os.path.join(classdir, file)
+            features = extractFeatures(path)
+            data.append (features)
+            labels.append(label)
+
+np.save(os.path.join(OUTDIR, 'X.npy'), np.array(data))
+np.save(os.path.join(OUTDIR, 'y.npy'), np.array(labels))
+print("✅ Features and labels saved")
